84dd4762c55a05cc8db58971ac07b5e55d4b8eb7
danielAdama <adamadaniel321@gmail.com>
2022-12-07 21:35:32 +0100

    Fixes

--- Ecommerce.Infrastructure/Services/Infrastructure/Persistence/Extensions.cs
+++ Ecommerce.Infrastructure/Services/Infrastructure/Persistence/Extensions.cs
@@ -26,54 +26,56 @@ namespace Ecommerce.Infrastructure.Services.Infrastructure.Persistence
 
         public static void Seed(this ModelBuilder modelBuilder) 
         {
-           // modelBuilder.Entity<Seller>()
-           //    .HasData(
-           //     new Seller { Id = 1, 
-           //         FullName = "Akachi Brown",
-           //         GenderType = Utilities.Enums.GenderTypeEnum.Male,
-           //         PictureUrl = "",
-           //         TimeCreated = DateTimeOffset.UtcNow,
-           //         TimeUpdated = DateTimeOffset.UtcNow
-           //     });
+            // modelBuilder.Entity<Category>()
+            //    .HasData(
+            //     new Seller { Id = 1, 
+            //         FullName = "Akachi Brown",
+            //         GenderType = Utilities.Enums.GenderTypeEnum.Male,
+            //         PictureUrl = "",
+            //         TimeCreated = DateTimeOffset.UtcNow,
+            //         TimeUpdated = DateTimeOffset.UtcNow
+            //     });
 
-           //modelBuilder.Entity<Product>()
-           //     .HasData(
-           //     new Product { Id = 1 ,
-           //         Name = "Asus", 
-           //         Price = 250.000, 
-           //         ProductCategory = Utilities.Enums.ProductCategoryEnum.Laptop, 
-           //         ProductLogo = "", 
-           //         ProductType = Utilities.Enums.ProductTypeEnum.Available, 
-           //         TimeUpdated = DateTimeOffset.UtcNow,
-           //         TimeCreated = DateTimeOffset.UtcNow
-           //     });
+            //modelBuilder.Entity<Product>()
+            //     .HasData(
+            //     new Product
+            //     {
+            //         Id = 1,
+            //         Name = "Asus",
+            //         Price = 250.000,
+            //         //ProductCategory = Utilities.Enums.ProductCategoryEnum.Laptop,
+            //         ProductImage = "",
+            //         IsAvailable = true,
+            //         TimeUpdated = DateTimeOffset.UtcNow,
+            //         TimeCreated = DateTimeOffset.UtcNow
+            //     });
 
-           // modelBuilder.Entity<Order>()
-           //     .HasData(
-           //     new Order
-           //     {
-           //         Id = 1,
-           //         TrackingId = Guid.NewGuid(),
-           //         UserId = 2,
-           //         Country = "Nigeria",
-           //         PhoneNumber = "+23456734567802",
-           //         TimeUpdated = DateTimeOffset.UtcNow,
-           //         TimeCreated = DateTimeOffset.UtcNow
-           //     });
+            //modelBuilder.Entity<Order>()
+            //    .HasData(
+            //    new Order
+            //    {
+            //        Id = 1,
+            //        TrackingId = Guid.NewGuid(),
+            //        UserId = 2,
+            //        Country = "Nigeria",
+            //        PhoneNumber = "+23456734567802",
+            //        TimeUpdated = DateTimeOffset.UtcNow,
+            //        TimeCreated = DateTimeOffset.UtcNow
+            //    });
 
 
-           // modelBuilder.Entity<OrderItem>()
-           //     .HasKey(k => new {k.ProductId, k.OrderId});
-           // modelBuilder.Entity<OrderItem>()
-           //     .HasData(
-           //     new OrderItem
-           //     {
-           //         Id = 1,
-           //         Amount = 2,
-           //         Price = 500.000,
-           //         TimeUpdated = DateTimeOffset.UtcNow,
-           //         TimeCreated = DateTimeOffset.UtcNow
-           //     });
+            //modelBuilder.Entity<OrderItem>()
+            //    .HasKey(k => new { k.ProductId, k.OrderId });
+            //modelBuilder.Entity<OrderItem>()
+            //    .HasData(
+            //    new OrderItem
+            //    {
+            //        Id = 1,
+            //        SelectedAmount = 2,
+            //        Price = 500.000,
+            //        TimeUpdated = DateTimeOffset.UtcNow,
+            //        TimeCreated = DateTimeOffset.UtcNow
+            //    });
         }
     }
 }
